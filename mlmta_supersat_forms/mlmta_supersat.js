b8d9451b7dfa53dee04e3190d8e2ba99c3c20e97
argiepiano <argiepiano@yahoo.com>
2018-12-12 13:24:53 -0700

    updated playathon

---
+++ mlmta_supersat_forms/mlmta_supersat.js
@@ -0,0 +1,39 @@
+(function ($) {
+  $(function() {
+    console.log('Here we go!');
+
+    $(".theory-test").on('change', function(event){
+      var value = $(this).val();
+      switch (value) {
+        case 'Piano':
+          setOptions(8);
+          break;
+        case 'Treble clef':
+        case 'Alto clef':
+        case 'Bass clef':
+        case 'Guitar':
+          setOptions(6);
+          break;
+        case 'Vocal bass clef':
+        case 'Vocal treble clef':
+          setOptions(4);
+      }      
+    });
+    
+    var setOptions = function(level) {
+      var levels = ['P', '1', '2', '3', '4', '5', '6', '7', '8'];
+      $(".theory-level").empty().append($('<option>', {
+          value: '_none',
+          text: '- Select -',
+        }));
+      for (var i=0; i<= level; i++) {
+        $(".theory-level").append($('<option>',{
+          value: i,
+          text: levels[i],
+        }));
+      }
+      $(".theory-level").trigger("chosen:updated");
+    }
+  });
+})(jQuery)
+
